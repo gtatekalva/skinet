fc885f4aa99949410a9f54c89204b41ca0b405bd
Guru Tatekalva <greddy@src1s.com>
2025-01-28 13:42:21 -0500

    section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+ 
+  baseURL = "https://localhost:5001/api/"
+  private http = inject(HttpClient);
   title = 'Skinet';
+  products: Product[] = [];
+
+  //this.products = response.data ,
+
+  ngOnInit(): void {
+    this.http.get<any>(this.baseURL + 'products').subscribe({
+      next: response =>   {
+        console.log('Full Response:', response.value.data);
+        this.products = response.value.data; // Extract the product array
+      },
+      error: err => console.error('Error fetching products:', err),
+      complete: () => console.log('Products fetched successfully')
+    });
+  }
+  
 }
